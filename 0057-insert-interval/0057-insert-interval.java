1b5551620d49d0e17e78857be3d9376de0dabe4a
YASHVI TAUNK <yashvitaunk@gmail.com>
2026-07-16 11:55:44 +0530

    Time: 1 ms (98.04%), Space: 47.1 MB (37.01%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,30 +1,50 @@
+import java.util.*;
+
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-         List<int[]> res = new ArrayList<>();
 
-         int i =0;
-         int n = intervals.length;
+        List<int[]> temp = new ArrayList<>();
 
-         while(i<n && intervals[i][1] < newInterval[0]){
-            res.add(intervals[i]);
+        boolean inserted = false;
 
-            i++;
+        // Insert new interval in sorted order
+        for (int[] curr : intervals) {
 
-         }
-         while(i<n &&  intervals[i][0] <=  newInterval[1]){
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1] , intervals[i][1]);
-            i++;
+            if (!inserted && newInterval[0] < curr[0]) {
+                temp.add(newInterval);
+                inserted = true;
+            }
 
-         }
-         res.add(newInterval);
+            temp.add(curr);
+        }
 
-         while(i<n){
-            res.add(intervals[i]);
-            i++;
-         }
+        // If new interval has the largest start
+        if (!inserted) {
+            temp.add(newInterval);
+        }
 
+        // Same merge logic as Question 56
+        List<int[]> res = new ArrayList<>();
 
-  return res.toArray(new int[res.size()][]);
+        int start = temp.get(0)[0];
+        int end = temp.get(0)[1];
+
+        for (int i = 1; i < temp.size(); i++) {
+
+            int s = temp.get(i)[0];
+            int e = temp.get(i)[1];
+
+            if (end >= s) {          // Overlap
+                end = Math.max(end, e);
+            } else {                 // No overlap
+                res.add(new int[]{start, end});
+                start = s;
+                end = e;
+            }
+        }
+
+        res.add(new int[]{start, end});
+
+        return res.toArray(new int[res.size()][]);
     }
 }
